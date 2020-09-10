4f8bae50bdf49acee31b9b969a6579d8e633ed57
AlphaDown <stoda2005@gmail.com>
2020-09-10 00:04:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#fadeIn").click(function(){
+    $(".greeting").FadeIn("slow");
+  })
+
+  $("#fadeOut").click(function(){
+    $(".greeting").FadeOut("slow");
+  })
+
+});
